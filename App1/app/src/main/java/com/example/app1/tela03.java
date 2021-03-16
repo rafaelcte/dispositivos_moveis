fe9538a1b1332e1a6d162023bfbd56d513010f02
rafael lopes de freitas <rafaelctepae@gmail.com>
2021-03-16 13:27:13 -0300

    trabalho finalizando

--- App1/app/src/main/java/com/example/app1/tela03.java
+++ App1/app/src/main/java/com/example/app1/tela03.java
@@ -2,7 +2,9 @@ package com.example.app1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class tela03 extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class tela03 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela03);
+    }
+    public void voltaLogin(View view){
+        Intent intent3 = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent3);
+    }
+    public void voltaSplash(View view){
+        Intent intent2 = new Intent(getApplicationContext(),SplashActivity.class);
+        startActivity(intent2);
     }
 }
